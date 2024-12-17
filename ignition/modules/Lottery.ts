562845d46fa317f0b4d9656f33d9be30d72ffe3b
th3gr33k <128638909+th3gr33k@users.noreply.github.com>
2024-12-17 12:10:55 +0000

    update deployment

--- ignition/modules/Lottery.ts
+++ ignition/modules/Lottery.ts
@@ -4,7 +4,6 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const TICKET_PRICE = ethers.parseEther("0.0001");
 const FEE_WALLET: string = "0x1b6A5eDd15c92B20837058Ae20A3106aC4778216";
 const VAULT_WALLET: string = "0x98AAA33dCDecD3452141342d825e1104830B9369";
-const BLOCK_CONFIRMATIONS = 5;
 
 
 const lotteryContract = buildModule("LockModule", (m) => {
